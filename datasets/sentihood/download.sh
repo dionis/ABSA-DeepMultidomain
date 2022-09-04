b9a061552bf80c016a650c5f69cc736aad7d3c50
Dionis <inoid2007@gmail.com>
2022-09-04 08:53:14 -0400

    Updated to load

--- datasets/sentihood/download.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget http://annotate-neighborhood.com/download/sentihood-train.json 
-wget http://annotate-neighborhood.com/download/sentihood-dev.json 
-wget http://annotate-neighborhood.com/download/sentihood-test.json 
